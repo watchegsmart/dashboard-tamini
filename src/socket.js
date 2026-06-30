593b842bcd3bb0a17ed4ef3b5b61e3f82db3f14c
watchegsmart <smyr91380@gmail.com>
2026-06-30 22:21:50 +0300

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 // src/socket.js
 import { io } from "socket.io-client";
 
-const SERVER_URL = process.env.REACT_APP_SERVER_URL || "https://tamini-server.onrender.com";
+const SERVER_URL = process.env.REACT_APP_SERVER_URL || "https://tamini-server-3.onrender.com";
 
 export const socket = io(SERVER_URL, {
   transports: ["websocket", "polling"],
